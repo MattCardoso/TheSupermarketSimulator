e40b33918d8652d8ad7d2ec2f039ff865c0bb5f8
MattCardoso <mat.cardoso25@gmail.com>
2015-06-11 13:10:26 -0300

    Created User class

---
+++ TheSupermarketSimulator/src/marketUser/User.java
@@ -0,0 +1,10 @@
+package marketUser;
+
+public class User {
+	private String _name;
+	private String _address;
+	private String _phone;
+	private String _email;
+	private String _id;
+	private String _password;
+}
