edf2234f56735c5744376f0450edc0d9457da761
MattCardoso <mat.cardoso25@gmail.com>
2015-06-11 14:16:20 -0300

    Created the classes that read and write on the .csv files

---
+++ TheSupermarketSimulator/src/marketCSV/ReadCSV.java
@@ -0,0 +1,56 @@
+package marketCSV;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+//import java.util.List;
+
+
+import marketProduct.Product;
+import marketUser.User;
+
+
+
+public class ReadCSV {
+
+	public static ArrayList<User> ReadCSVBook()
+	{
+		ArrayList<User> list = new ArrayList<User>();
+		
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("src/marketCSV/data/marketUsers.csv"));
+	        String data;
+	        while((data = br.readLine()) != null) 
+	            list.add(new User(data));
+	        
+	        br.close();
+		}
+		catch(IOException ex){}
+		
+		return list;
+	}
+	
+
+	
+	public static ArrayList<Product> ReadCSVUsers()
+	{
+		ArrayList<Product> list = new ArrayList<Product>();
+		
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("src/marketCSV/data/marketProducts.csv"));
+	        String data;
+	        while((data = br.readLine()) != null)
+	           	list.add(new Product(data));
+	        
+	        br.close();
+		}
+		catch(IOException ex){ex.printStackTrace();}
+		
+		return list;
+	}
+
+}
---
+++ TheSupermarketSimulator/src/marketCSV/WriteCSV.java
@@ -0,0 +1,51 @@
+package marketCSV;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import marketProduct.Product;
+import marketUser.User;
+
+public class WriteCSV {
+
+	//The comma that separates data in CSV.
+	//private static final String COMMA_DELIMITER = ",";
+	
+	public static void WriteCSVBook(ArrayList<Product> list)
+	{
+		try
+		{
+			FileWriter fw = new FileWriter("src/marketCSV/data/marketProducts.csv");
+			for(Product pr : list)
+			{
+				fw.append(pr.toString());
+				fw.append("\n");
+			}
+			
+			fw.flush();
+			fw.close();
+			
+		} catch(IOException ex){}
+	}
+	
+
+	
+	public static void WriteCSVUsers(ArrayList<User> list)
+	{
+		try
+		{
+			FileWriter fw = new FileWriter("src/marketCSV/data/marketUsers.csv");
+			for(User us : list)
+			{
+				fw.append(us.toString());
+				fw.append("\n");
+			}
+			
+			fw.flush();
+			fw.close();
+			
+		} catch(IOException ex){}
+	}
+}
