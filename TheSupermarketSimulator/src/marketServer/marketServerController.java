1b6e16e4980509a39f9fd9694d7b2459538873e4
Sabrina Faceroli <sabrina.tridico@usp.br>
2015-06-17 13:42:31 -0300

    Added control Buttons to Server

--- TheSupermarketSimulator/src/marketServer/marketServerController.java
+++ TheSupermarketSimulator/src/marketServer/marketServerController.java
@@ -1,5 +1,20 @@
 package marketServer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class marketServerController 
 {
-
+	@FXML
+	TextField ServerProdName_TextField, ServerProdPrice_TextField, ServerProdBestBef_TextField, 
+				ServerProdSuplier_TextField, ServerQuantity_TextField;
+	
+	@FXML
+	Button ServerCadastrar_Button, ServerSaveQty_Button;
+	
+	@FXML
+	ListView<?> ServerListStorage_ListView, ServerStorageProd_ListView, ServerSoldOutProd_ListView;
+	
 }
