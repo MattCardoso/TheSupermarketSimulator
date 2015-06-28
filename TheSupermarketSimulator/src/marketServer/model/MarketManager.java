866d65a28b4b5fee606378fd38bf4e8cb48cc4ec
MattCardoso <mat.cardoso25@gmail.com>
2015-06-27 22:27:12 -0300

    Changed class name of the Model section from the server Updated some features on the same Model

---
+++ TheSupermarketSimulator/src/marketServer/model/MarketManager.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package marketServer.model;
+
+import java.util.ArrayList;
+
+import marketProduct.Product;
+import marketServer.csv.ReadCSV;
+import marketUser.User;
+
+/**
+ * @author Mateus
+ *
+ */
+
+public class MarketManager {
+	private ArrayList<Product> 	_products;
+	private ArrayList<User>		_users;
+	
+	
+	public MarketManager(){
+		this._products 	= ReadCSV.ReadCSVProducts();
+		this._users 	= ReadCSV.ReadCSVUsers();
+	}
+	
+	public void add(Product p)	{_products.add(p);}
+	public void add(User u)		{_users.add(u);}
+	
+	public void updateQuantity(String prodName, int newQntt){
+		for (Product prod : _products) 
+			if(prod.getName().equals(prodName))
+				prod.setQuantity(newQntt);
+	}
+	
+	public ArrayList<Product> getProductList()	{return this._products;}
+	public ArrayList<User> getUserList()		{return this._users;}
+	
+	
+	public Product getProduct(String pname){
+		Product ret = null;
+		for (Product prd : _products) 
+			if(prd.getName().equals(pname))
+				ret = prd;
+		return ret;
+	}
+	
+	public User getUser(String uname){
+		User ret = null;
+		for (User usr : _users) {
+			if(usr.getName().equals(uname))
+				ret = usr;
+		}
+		return ret;
+	}
+	
+	
+}
