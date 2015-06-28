0768907f25c3ca9988de12a81ceaf083de3599aa
MattCardoso <mat.cardoso25@gmail.com>
2015-06-27 22:20:22 -0300

    Added getters and setter for the User class

--- TheSupermarketSimulator/src/marketUser/User.java
+++ TheSupermarketSimulator/src/marketUser/User.java
@@ -11,17 +11,35 @@ public class User {
 	// Contrutor usado para criar os usuários atraves do arquivo .csv
 	public User(String csv){
 		String[] values = csv.split(",");
-		this._name = values[0];
-		this._address = values[1];
-		this._phone = values[2];
-		this._email = values[3];
-		this._id = values[4];
-		this._password = values[5];
+		this.setName(values[0]);
+		this.setAddress(values[1]);
+		this.setPhone(values[2]);
+		this.setEmail(values[3]);
+		this.setID(values[4]);
+		this.setPassword(values[5]);
 	}
+
+	public String getName() 					{return _name;}
+	public void setName(String _name)			{this._name = _name;}
+
+	public String getAddress() 					{return _address;}
+	public void setAddress(String _address) 	{this._address = _address;}
+		
+	public String getPhone() 					{return _phone;}
+	public void setPhone(String _phone) 		{this._phone = _phone;}
+
+	public String getEmail() 					{return _email;}
+	public void setEmail(String _email) 		{this._email = _email;}
+
+	public String getID() 						{return _id;}
+	public void setID(String _id) 				{this._id = _id;}
+
+	public String getPassword() 				{return _password;}
+	public void setPassword(String _password) 	{this._password = _password;}
 	
-	public String toString(){
-		return "" + _name + "," + _address + "," + _phone + "," + _email +
-				"," + _id + "," + _password; // + "," _quantity; 
-	}
+//	public String toString(){
+//		return "" + _name + "," + _address + "," + _phone + "," + _email +
+//				"," + _id + "," + _password; // + "," _quantity; 
+//	}
 	
 }
