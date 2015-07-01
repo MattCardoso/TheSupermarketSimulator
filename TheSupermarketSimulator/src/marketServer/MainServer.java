36868abe01dc8e690ebd231e64d01d894995bd10
Sabrina Faceroli <sabrina.tridico@usp.br>
2015-07-01 13:33:54 -0300

    Mains created

---
+++ TheSupermarketSimulator/src/marketServer/MainServer.java
@@ -0,0 +1,36 @@
+package marketServer;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class MainServer extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		primaryStage.setTitle("T H E * S U P E R M A R K E T * S I M U L A T O R");
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(MainServer.class.getResource("marketServer.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Scene scene = new Scene(page);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+		//Main TODO here.
+	}
+	
+	public MainServer()
+	{}
+}
